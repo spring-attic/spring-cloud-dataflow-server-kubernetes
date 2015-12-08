b677144e6cdf11cdb57e771ac2739245538849c6
Eric Bottard <eric.bottard+github@gmail.com>
2015-12-08 17:00:04 +0100

    Add implementation

---
+++ spring-cloud-dataflow-admin-kubernetes/src/main/java/org/springframework/cloud/dataflow/admin.spi.kubernetes/KubernetesModuleDeployerConfiguration.java
@@ -0,0 +1,72 @@
+package org.springframework.cloud.dataflow.admin.spi.kubernetes;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.dataflow.module.deployer.ModuleDeployer;
+import org.springframework.cloud.dataflow.module.deployer.kubernetes.ContainerFactory;
+import org.springframework.cloud.dataflow.module.deployer.kubernetes.DefaultContainerFactory;
+import org.springframework.cloud.dataflow.module.deployer.kubernetes.KubernetesModuleDeployer;
+import org.springframework.cloud.dataflow.module.deployer.kubernetes.KubernetesModuleDeployerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+/**
+ * Spring Bean configuration for the {@link KubernetesModuleDeployer}.
+ * @author Florian Rosenberg
+ */
+@Configuration
+@EnableConfigurationProperties(KubernetesModuleDeployerProperties.class)
+public class KubernetesModuleDeployerConfiguration {
+	
+	@Autowired
+	private KubernetesModuleDeployerProperties properties;
+	
+	@Bean
+	public ModuleDeployer processModuleDeployer() {
+		return new KubernetesModuleDeployer(properties);
+	}
+
+	@Bean
+	public ModuleDeployer taskModuleDeployer() {
+		return processModuleDeployer();
+	}
+
+	@Bean
+	public KubernetesClient kubernetesClient() {
+		return new DefaultKubernetesClient();
+	}
+
+	@Bean
+	public ContainerFactory containerFactory() {
+		return new DefaultContainerFactory();
+	}
+
+	@Profile("cloud")
+	@AutoConfigureBefore(RedisAutoConfiguration.class)
+	protected static class RedisConfig {
+
+		@Bean
+		public Cloud cloud(CloudFactory cloudFactory) {
+			return cloudFactory.getCloud();
+		}
+
+		@Bean
+		public CloudFactory cloudFactory() {
+			return new CloudFactory();
+		}
+
+		@Bean
+		RedisConnectionFactory redisConnectionFactory(Cloud cloud) {
+			return cloud.getSingletonServiceConnector(RedisConnectionFactory.class, null);
+		}
+	}
+}
---
+++ spring-cloud-dataflow-admin-kubernetes/src/main/java/org/springframework/cloud/dataflow/module/deployer/kubernetes/KubernetesModuleInstanceStatus.java
@@ -0,0 +1,85 @@
+package org.springframework.cloud.dataflow.module.deployer.kubernetes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.fabric8.kubernetes.api.model.ContainerStatus;
+import io.fabric8.kubernetes.api.model.Pod;
+
+import org.springframework.cloud.dataflow.module.DeploymentState;
+import org.springframework.cloud.dataflow.module.ModuleInstanceStatus;
+
+/**
+ * Represents the status of a module.
+ * @author Florian Rosenberg
+ */
+public class KubernetesModuleInstanceStatus implements ModuleInstanceStatus {
+
+	private final Pod pod;
+	private final String moduleId;
+	private ContainerStatus containerStatus;
+
+	public KubernetesModuleInstanceStatus(String moduleId, Pod pod) {
+		this.moduleId = moduleId;
+		this.pod = pod;
+		// we assume one container per pod
+		if (pod != null && pod.getStatus().getContainerStatuses().size() == 1) {
+			this.containerStatus = pod.getStatus().getContainerStatuses().get(0);
+		} else {
+			this.containerStatus = null;
+		}
+	}
+
+	@Override
+	public String getId() {
+		return String.format("%s:%s", moduleId, pod.getMetadata().getName());
+	}
+
+	@Override
+	public DeploymentState getState() {
+		return pod != null && containerStatus != null ? mapState() : DeploymentState.unknown;
+	}
+
+	// Maps Kubernetes phases onto Spring Cloud Dataflow states
+	private DeploymentState mapState() {
+		
+		switch (pod.getStatus().getPhase()) {
+			
+			case "Pending":
+				return DeploymentState.deploying;
+				
+			// We only report a module as running if the container is also ready to service requests.
+			// We also implement the Readiness check as part of the container to ensure ready means
+			// that the module is up and running and not only that the JVM has been created and the
+			// Spring module is still starting up
+			case "Running":
+				// we assume we only have one container
+				if (containerStatus.getReady())
+					return DeploymentState.deployed;
+				else
+					return DeploymentState.deploying;
+
+			case "Failed":
+				return DeploymentState.failed;
+
+			case "Unknown":
+				return DeploymentState.unknown;
+
+			default: 
+				return DeploymentState.unknown;
+		}
+	}
+
+	@Override
+	public Map<String, String> getAttributes() {
+		Map<String, String> result = new HashMap<>();
+
+		result.put("pod_starttime", pod.getStatus().getStartTime());
+		result.put("pod_ip", pod.getStatus().getPodIP());
+		result.put("host_ip", pod.getStatus().getHostIP());
+		result.put("container_restart_count", ""+ containerStatus.getRestartCount());
+
+		// TODO add more useful stuff
+		return result;
+	}
+}
