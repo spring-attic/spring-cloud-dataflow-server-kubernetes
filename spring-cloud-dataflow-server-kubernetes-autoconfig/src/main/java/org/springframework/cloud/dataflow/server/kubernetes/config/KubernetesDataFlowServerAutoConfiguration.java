4aecd62f14a10b5d46b4fc91d9cdc17d9bbac9dc
Thomas Risberg <trisberg@pivotal.io>
2016-11-21 16:54:52 -0500

    Change package name for KubernetesDataFlowServerAutoConfiguration

--- spring-cloud-dataflow-server-kubernetes-autoconfig/src/main/java/org/springframework/cloud/dataflow/server/kubernetes/config/KubernetesDataFlowServerAutoConfiguration.java
+++ spring-cloud-dataflow-server-kubernetes-autoconfig/src/main/java/org/springframework/cloud/dataflow/server/kubernetes/config/KubernetesDataFlowServerAutoConfiguration.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.cloud.dataflow.server.config.kubernetes;
+package org.springframework.cloud.dataflow.server.kubernetes.config;
 
 import java.util.HashMap;
 import java.util.Map;
