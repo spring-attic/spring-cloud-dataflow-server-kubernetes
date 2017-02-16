458172db398fe12d1ef93e256f69b8b5d7b0a531
Thomas Risberg <trisberg@pivotal.io>
2017-02-16 12:32:21 -0500

    Update for SCDF 1.2 changes regarding --metedata-uri for apps
    - use SCDF 1.2.0.BUILD-SNAPSHOT to pick up latest fixes
    
    - adding a MavenResourceLoader to resolve maven resources for app metadata

--- spring-cloud-dataflow-server-kubernetes-autoconfig/src/main/java/org/springframework/cloud/dataflow/server/kubernetes/config/KubernetesDataFlowServerAutoConfiguration.java
+++ spring-cloud-dataflow-server-kubernetes-autoconfig/src/main/java/org/springframework/cloud/dataflow/server/kubernetes/config/KubernetesDataFlowServerAutoConfiguration.java
@@ -21,6 +21,8 @@ import java.util.Map;
 
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.cloud.deployer.resource.docker.DockerResourceLoader;
+import org.springframework.cloud.deployer.resource.maven.MavenProperties;
+import org.springframework.cloud.deployer.resource.maven.MavenResourceLoader;
 import org.springframework.cloud.deployer.resource.support.DelegatingResourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,10 +39,12 @@ import org.springframework.core.io.ResourceLoader;
 public class KubernetesDataFlowServerAutoConfiguration {
 
 	@Bean
-	public DelegatingResourceLoader delegatingResourceLoader() {
+	public DelegatingResourceLoader delegatingResourceLoader(MavenProperties mavenProperties) {
 		DockerResourceLoader dockerLoader = new DockerResourceLoader();
+		MavenResourceLoader mavenLoader = new MavenResourceLoader(mavenProperties);
 		Map<String, ResourceLoader> loaders = new HashMap<>();
 		loaders.put("docker", dockerLoader);
+		loaders.put("maven", mavenLoader);
 		return new DelegatingResourceLoader(loaders);
 	}
 }
