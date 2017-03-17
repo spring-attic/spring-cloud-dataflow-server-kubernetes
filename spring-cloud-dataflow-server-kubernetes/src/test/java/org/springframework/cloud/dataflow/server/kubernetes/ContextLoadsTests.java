7805355b618c2d6345cb549e2d821c41440715d2
Thomas Risberg <trisberg@pivotal.io>
2017-03-17 16:18:52 -0400

    Update to latest snapshot of Data Flow core
    - this updates to Boot 1.5.x
    
    Resolves #168

--- spring-cloud-dataflow-server-kubernetes/src/test/java/org/springframework/cloud/dataflow/server/kubernetes/ContextLoadsTests.java
+++ spring-cloud-dataflow-server-kubernetes/src/test/java/org/springframework/cloud/dataflow/server/kubernetes/ContextLoadsTests.java
@@ -17,9 +17,7 @@ package org.springframework.cloud.dataflow.server.kubernetes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
