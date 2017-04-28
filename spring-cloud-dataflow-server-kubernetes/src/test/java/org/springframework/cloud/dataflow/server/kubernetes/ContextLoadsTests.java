51dd0ec413569bf921d21e7a496c870f76927066
Thomas Risberg <trisberg@pivotal.io>
2017-04-28 14:05:18 -0400

    Adapting to new checkstyle and code formatting rules
    Resolves #178

--- spring-cloud-dataflow-server-kubernetes/src/test/java/org/springframework/cloud/dataflow/server/kubernetes/ContextLoadsTests.java
+++ spring-cloud-dataflow-server-kubernetes/src/test/java/org/springframework/cloud/dataflow/server/kubernetes/ContextLoadsTests.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@ package org.springframework.cloud.dataflow.server.kubernetes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,9 +25,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KubernetesDataFlowServer.class)
 public class ContextLoadsTests {
 
-    @Test
-    public void contextLoads() {
+	@Test
+	public void contextLoads() {
 
-    }
+	}
 
 }
